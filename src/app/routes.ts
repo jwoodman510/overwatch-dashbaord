65e07b73796509f6f5c99c8f15c9b66f650e84fa
Jordan Woodman <jordan.woodman@bluesombrero.com>
2018-11-16 18:15:53 -0500

    Update base route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,14 +1,8 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'dashboard'
-  },
-  {
-    path: 'dashboard',
     children: [
       {
         path: '',
