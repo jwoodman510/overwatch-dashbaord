bf2430b666f2345d2da9b78f493b2d88dfbd3f02
Jordan Woodman <jordan.woodman@bluesombrero.com>
2018-11-16 20:32:16 -0500

    fix api urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   overwatchApi: {
-    host: 'http://ec2-52-43-227-144.us-west-2.compute.amazonaws.com/'
+    host: 'http://ec2-52-43-227-144.us-west-2.compute.amazonaws.com/api/'
   }
 };
