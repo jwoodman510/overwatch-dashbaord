b703ff365dadce126c6ba19b1774285a41f85d81
Jordan Woodman <jordan.woodman@bluesombrero.com>
2018-11-16 20:47:17 -0500

    add error handling

--- src/app/services/stats.data.service.ts
+++ src/app/services/stats.data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Environment, PlayerStats } from '../models';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class StatsDataService {
@@ -18,6 +18,7 @@ export class StatsDataService {
     const bt = battleTag.replace('#', '-');
 
     return this.http.get<any>(`${this.baseUrl}${platform}/${region}/${bt}`).pipe(
+      catchError(() => this.http.get<any>(`https://ow-api.com/v1/stats/${platform}/${region}/${bt}/profile`)),
       map(x => x.error && x.error === 'Player not found' ? undefined : x as PlayerStats),
       tap(x => {
         if (x) {
